b8a7ee4172678878623d811c16e767450a2e7c44
Jonathan Cubides <prieto.jona@gmail.com>
2026-07-07 12:13:27 -0500

    feat: core package skeleton

---
+++ Grip.lean
@@ -0,0 +1,14 @@
+/-
+Copyright 2026 Jonathan Cubides. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
+/-!
+# grip
+
+A fast, graded Lean 4 byte-parser library. Public modules are re-exported here as
+they land. Milestone 0 is the skeleton; combinators arrive in Milestone 1.
+-/
+
+namespace Grip
+end Grip
