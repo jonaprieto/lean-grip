3925eeac0675f1d0f00b1eefced381664c2c6cba
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-03 08:29:34 -0500

    chore: standardize author headers

--- examples/Lambda.lean
+++ examples/Lambda.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Jonathan Cubides. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jonathan Cubides
+Authors: Jonathan Prieto-Cubides
 
 Adapted from Examples/Lambda.lean in prim-parser by Jan Mas Rovira
 (https://github.com/janmasrovira/prim-parser, commit 30d0ad8, 2026-05-08):
--- examples/Sexp.lean
+++ examples/Sexp.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Jonathan Cubides. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jonathan Cubides
+Authors: Jonathan Prieto-Cubides
 
 The grammar and `atom`/`list` structure follow Examples/SExp.lean in
 prim-parser by Jan Mas Rovira (https://github.com/janmasrovira/prim-parser,
